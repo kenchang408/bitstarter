a625c2a323bcf93ff367550c23946a7f58af8c80
Ubuntu <ubuntu@ip-172-31-42-161.us-west-2.compute.internal>
2013-07-12 07:50:07 +0000

    modified grader to read url and added urlpage.html

--- grader.js
+++ grader.js
@@ -1,13 +1,16 @@
-!/usr/bin/env node
+#!/usr/bin/env node
 
 /*automatically grade files for the presence of specified HTML tags/attributes. Uses commander.js and cheerio. Teaches command line application development and basic DOM parsing.
 */
 
+var util = require('util');
 var fs = require('fs');
-var program = require('commander';
+var rest = require('restler');
+var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -26,7 +29,7 @@ var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
     };
 
-var checkHtmlFile = function(htmlfile, checkfile) {
+var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
     var checks = loadChecks(checksfile).sort();
     var out = {};
@@ -43,14 +46,44 @@ var clone = function(fn) {
     return fn.bind({});
     };
 
+var checkWebPage = function(web_url, checksfile){
+        
+    rest.get(web_url).on('complete', function(result){
+        if(result instanceof Error){
+            console.log("%s cannot be found: %s", instr, result.message);
+            process.exit(1);
+            }
+        else{
+	    fs.writeFileSync("urlpage.html", result);
+	    var html = "urlpage";
+	    var checkJson = checkHtmlFile(html, program.checks);
+	    var outJson = JSON.stringify(checkJson, null, 4);
+	    console.log(outJson);
+	    
+            }
+	             
+            
+        });
+};
+
+
 if(require.main == module) {
     program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <url>', "URL to page to test", String)
     .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
-    } else {
+    if (program.url){
+	var web_url=program.url.toString();
+	checkWebPage(web_url, program.checks);
+	
+	}
+    else {
+	var checkJson = checkHtmlFile(program.file, program.checks);
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+	}
+    } 
+else {
 	exports.checkHtmlFile = checkHtmlFile;
 }
